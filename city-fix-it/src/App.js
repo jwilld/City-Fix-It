280163a4d603c8d2adc72c5f30739ddcdd804baf
Jerry Davidson <willdjerry@gmail.com>
2019-08-19 14:49:48 -0400

    made the landing page have its own background

--- city-fix-it/src/App.js
+++ city-fix-it/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom'
 import Landing from './landing-page/landing'
+import Main  from './main-page/main'
 
 function App() {
   return (
     <div className="App">
-      <Route path ='/' component = {Landing}/>
+      <Route exact path ='/' component = {Landing}/>
+      <Route exact path='/main/' component = {Main}/>
+
     </div>
   );
 }
