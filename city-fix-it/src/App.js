8f9c7ea4d7551cdf3b2c5570ab7b6cb4d8bd1720
Jerry Davidson <willdjerry@gmail.com>
2019-08-19 13:51:44 -0400

    landing page show title

--- city-fix-it/src/App.js
+++ city-fix-it/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+      <h1>City Fix-It</h1>
+      <p>Welcome to City Fix-It, where you can report damage to the city.</p>
+      </div>
     </div>
   );
 }
