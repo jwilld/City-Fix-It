3706163af981555246b8de11c7050ebfad824e17
Jerry Davidson <willdjerry@gmail.com>
2019-08-19 13:59:03 -0400

    landing page added as separate component

--- city-fix-it/src/App.js
+++ city-fix-it/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './landing-page/landing'
 
 function App() {
   return (
     <div className="App">
-      <div className='title'>
-      <h1>City Fix-It</h1>
-      <p>Welcome to City Fix-It, where you can report damage to the city.</p>
-      </div>
+      <Landing/>
     </div>
   );
 }
