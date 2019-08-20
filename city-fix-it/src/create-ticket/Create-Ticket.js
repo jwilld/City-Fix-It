88cba49522eeac2097e76b2f7412ea8bf47960e7
Jerry Davidson <willdjerry@gmail.com>
2019-08-20 10:04:04 -0400

    create ticket mock made

--- city-fix-it/src/create-ticket/Create-Ticket.js
+++ city-fix-it/src/create-ticket/Create-Ticket.js
@@ -5,16 +5,37 @@ class CreateTicket extends Component {
   render() {
     return (
       <div className="create-form">
-        <form className="form-input">
+        <form className="form">
         <div className='form-label'>
-          <label>Create Ticket</label>
+          <label >Create Ticket</label>
         </div>
+        <div className = 'form-inputs'>
           <div>
             <input type="text" />
           </div>
           <div>
             <input type="text" />
           </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+              <input type="submit"/>
+          </div>
+
+        </div>
         </form>
       </div>
     );
