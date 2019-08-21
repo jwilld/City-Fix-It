8f769ce8cc46b883fdee701e4e23ece675f8f139
Jerry Davidson <willdjerry@gmail.com>
2019-08-21 10:19:09 -0400

    FormData shows information put in the current form

--- city-fix-it/src/create-ticket/Create-Ticket.js
+++ city-fix-it/src/create-ticket/Create-Ticket.js
@@ -1,42 +1,55 @@
 import React, { Component } from "react";
 import "./create-ticket.css";
+import axios from 'axios'
 
 
 class CreateTicket extends Component {
+  constructor(){
+    super()
+    this.state = {
+      Address:'',
+      Type:'',
+      Priority:'',
+      Description:'',
+      Status:'',
+    }
+  }
+  
+  createTicket = event => {
+    event.preventDefault()
+    let form = new FormData(event.target)
+    let newTicket = {}
+    let keys = Object.keys(this.state)
+    keys.forEach(key => (newTicket[key] = form.get(key)));
+    console.log(newTicket)
+    const url = 'https://city-fix-it.herokuapp.com/tickets'
+    axios.post(url).then(response =>{
+      console.log(response.data)
+    })
+  }
+
+
   render() {
+    let keys = Object.keys(this.state)
+
+    let headers = ['Address','Type','Priority','Description','Status']
+    let createForm = headers.map(name =>(
+      <div>
+        <span>{name}</span>
+        <input type="text" name={name}/>
+      </div>
+    ))
     return (
-      <div className="create-form">
+      <div className="create-form" onSubmit={this.createTicket}>
         <form className="form">
         <div className='form-label'>
           <label >Create Ticket</label>
         </div>
-        <div className = 'form-inputs'>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-              <input type="submit"/>
-          </div>
-
+        <div>
+          {createForm}
+          <input type="submit" value="submit"/>
         </div>
+        
         </form>
       </div>
     );
