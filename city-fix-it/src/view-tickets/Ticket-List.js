faf4fc37536404232f25ef06e88f00dd2ffa5c45
Jamie Miller <miller0328@gmail.com>
2019-08-21 10:29:16 -0400

    still in the works

--- city-fix-it/src/view-tickets/Ticket-List.js
+++ city-fix-it/src/view-tickets/Ticket-List.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react"
-
-const url = "https://city-fix-it.herokuapp.com/tickets"
+import { getPriority } from "os";
 
 class TicketList extends Component {
-    axios
-    .get(url)
-    .then(response => {
-        let urgentArray = []
-        
-    })
-
-
+    constructor() {
+        super();
+        this.state = {
+            urgentArray = [],
+            routineArray = []
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick = event => {
+        event.preventDefault()
+        const url = "https://city-fix-it.herokuapp.com/tickets"
+        axios.get(url).then(response => {
+            this.setState({
+                urgentArray = this.state.priority.filter(function(ticket) {
+                    if (priority === urgent) {
+                        ticket.push(urgentArray)
+                        return urgentArray
+                    } else {
+                        (priority === routine); {
+                            ticket.push(routineArray)
+                            return routineArray
+                        }
+                    }
+                })
+            })
+            // console.log(response.data)
+        })
+    }
     render() {
-        return(
-
+        return (
+            <div className="viewUrgentTickets">
+                <button onClick={this.handleClick}>
+                    Urgent
+                </button>
+            </div>
         )
     }
 }
