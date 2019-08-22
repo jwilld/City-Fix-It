a0ea8a71efc6fdd0933112c36e9fb0d1d656bd9e
Jerry Davidson <willdjerry@gmail.com>
2019-08-22 11:26:46 -0400

    added create ticket back

--- city-fix-it/src/ticket-search/ticket-search.js
+++ city-fix-it/src/ticket-search/ticket-search.js
@@ -1,12 +1,51 @@
 import React, { Component } from "react";
 import "./ticket-search.css";
+import axios from "axios";
+import { Link } from 'react-router-dom'
 
 class TicketSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tickets: []
+    };
+  }
+
+  componentDidMount = () => {
+    const url = "https://city-fix-it.herokuapp.com/tickets";
+    axios.get(url).then(response => {
+      this.setState({ tickets: response.data });
+    });
+  };
+
   render() {
-    return (
+    console.log(this.state.tickets);
+    let ticketList = this.state.tickets.map(ticket => (
       <div>
+        <div className="list-item-grid">
+      
+          <div className="urgent-item">
+            <Link to='/main/view/urgent/edit'class='edit-link'>
+            <div className='edit display'>Edit</div>
+            </Link>
+            <div className="type display">{ticket.type}</div>
+            <div className="priority display ">{ticket.priority}</div>
+            <div className="status display">{ticket.status}</div>
+          </div>
+        </div>
+      </div>
+    ));
+    return (
+      <div className="ticket-search">
         <div>
-          <input className='search-box' type="text" placeholder="serach for ticket" />
+          <input
+            className="search-box"
+            type="text"
+            placeholder="Search for a ticket"
+          />
+        </div>
+        <div>
+          {ticketList}
         </div>
       </div>
     );
--- city-fix-it/src/view-tickets/View-Ticket.js
+++ city-fix-it/src/view-tickets/View-Ticket.js
@@ -33,24 +33,10 @@ class ViewTicket extends Component {
     return (
       <div className="two-buttons">
         <div className="link-grid">
-          <Link to="/main/view/urgent" className="link">
-            <button className="urgent-button view">Urgent</button>
-          </Link>
-          <Link to="/main/view/routine" className="link">
-            <button className="routine-button view">Routine</button>
-          </Link>
         </div>
         <div className="ticket-view-grid">
           <Route path="/main/view/urgent/edit"
           render={() => <FormChange/>}/>
-          <Route
-            path="/main/view/urgent"
-            render={() => <TicketList status={this.state.urgentArray} />}
-          />
-          <Route
-            path="/main/view/routine"
-            render={() => <RoutineList status={this.state.routineArray} />}
-          />
         </div>
       </div>
     );
