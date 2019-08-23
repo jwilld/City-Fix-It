b1fbfdbf9011239fe1b2f081ba9cd37ddc099144
Jamie Miller <miller0328@gmail.com>
2019-08-23 09:35:56 -0400

    edit ticket chart

--- src/main-page/main.js
+++ src/main-page/main.js
@@ -17,11 +17,10 @@ class Main extends Component {
         <div className="dash" />
 
         <div className="main-content">
-          <Route path = '/main/ticket-search' component={TicketSearch}/>
+          <Route path ='/main/ticket-search' component={TicketSearch}/>
           <Route path ='/main/create' component={CreateTicket}/> 
           <Dashboard />
-          <TicketChart/>
-
+          <Route exact path='/main' component={TicketChart}/>
         </div>
       </div>
     );
