00410f6118ac8d36b771a05494f3b13baf77c55b
Jerry Davidson <willdjerry@gmail.com>
2019-09-27 11:16:46 -0400

    admin and user text shows on landing page

--- src/landing-page/landing.js
+++ src/landing-page/landing.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./landing.css";
+import UserType from '../user-type/UserType'
 
 class Landing extends Component {
   render() {
     return (
       <div className='landing-container'>
         <div className="title">
+          <div>
           <Link to="/main/" className="landing-link">
             <h1>City Fix-It</h1>
           </Link>
+          </div>
+          <div className='description'>
           <p>
             Welcome to City Fix-It, where you can report damage to the city.
           </p>
+
+          </div>
+        </div>
+        <div className='user-type-container'>
+        <UserType/>
         </div>
       </div>
     );
